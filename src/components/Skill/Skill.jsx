a3d550f4896957639de8147bef7693e128c07bca
kaliptoDev <121462067+kaliptoDev@users.noreply.github.com>
2023-08-09 14:25:18 +0200

    Added icons for Skills section

--- src/components/Skill/Skill.jsx
+++ src/components/Skill/Skill.jsx
@@ -1,19 +1,26 @@
 import './Skill.css'
+import useCurrentLanguage from '../../hooks/useCurrentLanguage'
+import { useEffect, useState } from 'react'
 
-const Skill = ({ skill }) => {
-// console.log(skill)
-    const { name, description, icon } = skill ? skill : { name: null, description: null, icon: null}
+const Skill = ({ skill, needToClose, setNeedToClose }) => {
+
+    const { currentLanguage } = useCurrentLanguage()
+    skill = JSON.parse(skill)
+
+
 
     return (
         <div className="skill">
-            <div className="skill__icon">
-                <img src={icon} alt={name} />
+            <div className="skill__title" onClick={handleOpen}>
+                <div className="skill__icon__div">
+                    <img src={skill && "/icons/" + skill.icon + ".svg"} alt={skill && skill.name[currentLanguage]} className='skill__icon' />
+                </div>
+                <div className="skill__name">
+                    <h3>{skill && skill.name[currentLanguage]}</h3>
+                </div>
             </div>
-            <div className="skill__name">
-                <h3>{name}</h3>
-            </div>
-            <div className="skill__description">
-                <p>{description}</p>
+            <div className="skill__description descClosed">
+                <p>{skill && skill.description[currentLanguage]}</p>
             </div>
         </div>
     )
--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,18 +1,21 @@
 import './Skills.css'
 import Skill from '../Skill/Skill'
 import useSkills from '../../hooks/useSkills'
-
+import { useState } from 'react'
 
 const Skills = () => {
 
     const { skills } = useSkills()
-    const knowledge = skills? skills.knowledge : null
-    const softSkills = skills? skills.softSkills : null
-    knowledge? knowledge.map((skill, index) => console.log(skill)) : null
+    const knowledge = skills ? skills.knowledge : null
+    const softSkills = skills ? skills.softSkills : null
+    // knowledge? knowledge.map((skill, index) => console.log(skill)) : null
+
+    const [needToClose, setNeedToClose] = useState(false)
+
     return (
         <section className="skills">
             <div className="skills__container__one">
-            {/* {knowledge ? knowledge.map((skill, index) => <Skill key={index} skill={skill} />) : null} */}
+                {knowledge ? knowledge.map((skill, index) => <Skill key={index} skill={JSON.stringify(skill)} needToClose={needToClose} setNeedToClose={setNeedToClose}/>) : null}
             </div>
             <div className="skills__container__two">
             </div>
