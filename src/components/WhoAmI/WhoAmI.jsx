91633796a7275de68a84599d8f8898c40eb8a8c7
kaliptoDev <baptiste.andre17@gmail.com>
2023-08-12 03:14:23 +0200

    Who Am I page added

---
+++ src/components/WhoAmI/WhoAmI.jsx
@@ -0,0 +1,31 @@
+import './WhoAmI.css'
+import useCurrentLanguage from '../../hooks/useCurrentLanguage'
+import usePageContent from '../../hooks/usePageContent'
+
+const WhoAmI = () => {
+
+    const { currentLanguage } = useCurrentLanguage()
+    const { pageContent } = usePageContent()
+
+
+    return (
+        <>
+            <div className="whoami_link" id='whoami'>
+                <div className="whoami">
+                    <div className='whoami__content'>
+                        <div className='whoami__content__div'>
+                            <img className='whoami__img' src='/images/portrait.jpg' alt={pageContent && pageContent.whoami.title[currentLanguage]} />
+                            <p className='whoami__content__desc'>
+                                <span className='span__first span__whoami'>«</span>
+                                {pageContent ? pageContent.whoami.content[currentLanguage] : null}
+                                <span className='span__last span__whoami'>»</span>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default WhoAmI
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,12 +2,14 @@ import './Home.css'
 import Intro from '../../components/Intro/Intro'
 import Skills from '../../components/Skills/Skills'
 import Example from '../../components/Example/Example'
+import WhoAmI from '../../components/WhoAmI/WhoAmI'
 
 const Home = () => {
 
     return (
         <>
             <Intro />
+            <WhoAmI />
             <Example />
             {/* <Skills /> */}
         </>
