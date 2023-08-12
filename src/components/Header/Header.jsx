450509b942480f97cb7aef8c8c540f2af856cd10
kaliptoDev <baptiste.andre17@gmail.com>
2023-08-12 03:14:09 +0200

    Header Modified and fixed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -19,6 +19,7 @@ const Header = () => {
     const [reduceMenuButtonClasses, setReduceMenuButtonClasses] = useState('transparent ')
     const [navBackgroundClasses, setNavBackgroundClasses] = useState('')
     const [arrowClasses, setArrowClasses] = useState('')
+    const [titleClasses, setTitleClasses] = useState('')
 
     const handleChangeHeight = () => {
         const height = window.scrollY
@@ -47,7 +48,7 @@ const Header = () => {
     }
 
     useEffect(() => {
-        if (isMobile && isUnderIntro && !navClasses.includes('reduced')) {toggleMenu()}
+        if (isMobile && isUnderIntro && !navClasses.includes('reduced')) { toggleMenu() }
         if (!logoClasses.includes('noShadow') && !isUnderIntro) setLogoClasses(''.concat(logoClasses, 'noShadow '))
     }, [isUnderIntro])
 
@@ -94,11 +95,14 @@ const Header = () => {
 
         if (shouldLogoShrink) {
             setLogoClasses(logoClasses.replace('header__logo__unshrinked ', ''))
+            setTitleClasses(''.concat(titleClasses, 'header__title__div__disappear '))
 
         } else {
             setLogoClasses(logoClasses.replace('noShadow ', ''))
+            setTitleClasses(titleClasses.replace('header__title__div__disappear ', ''))
             if (!logoClasses.includes('header__logo__unshrinked')) {
                 setLogoClasses(''.concat(logoClasses, 'header__logo__unshrinked '))
+                
             }
         }
     }, [shouldLogoShrink])
@@ -120,11 +124,16 @@ const Header = () => {
 
     return (
         <>
-            <header className="header">
+            <header className="header" id='home'>
                 <div className={`${logoClasses} header__logo__div`}>
-                    <img src='/logos/logo.svg' alt='logo' className='header__logo' />
+                    <a href="#home" className='header__logo__link'>
+                        <img src='/logos/logo.svg' alt='logo' className='header__logo' />
+                    </a>
                 </div>
-
+                <div className={`${titleClasses} header__title__div`}>
+                    <h1 className='header__title'>{pageContent ? header.title[currentLanguage] : "default"}</h1>
+                    <h2 className='header__subtitle'>{pageContent ? header.subtitle[currentLanguage] : "default"} <span className='blue'>Fr</span><span className='white'>an<span className='red'>ce</span></span></h2>
+                </div>
                 <div className={navBackgroundClasses + ' header__background '}></div>
 
                 <button className={`${reduceMenuButtonClasses} reduceMenu__button`} onClick={handleToggleMenu}>
@@ -132,11 +141,11 @@ const Header = () => {
                 </button>
                 <nav className={`${navClasses} header__nav `}>
                     <ul className='nav__list'>
-                        <li><a href="/" className='header__home header__nav__item'>{pageContent ? nav.home[currentLanguage] : "default"}</a></li>
-                        <li><a href="/whoami" className='header__whoami header__nav__item '>{pageContent ? nav.skills[currentLanguage] : "default"}</a></li>
-                        <li><a href="/portfolio" className='header__portfolio header__nav__item'>{pageContent ? nav.portfolio[currentLanguage] : "default"}</a></li>
-                        <li><a href="/pricing" className='header__pricing header__nav__item'>{pageContent ? nav.pricing[currentLanguage] : "default"}</a></li>
-                        <li><a href="/contact" className='header__contact header__nav__item'>{pageContent ? nav.contact[currentLanguage] : "default"}</a></li>
+                        <li><a href="#home" className='header__home header__nav__item'>{pageContent ? nav.home[currentLanguage] : "default"}</a></li>
+                        <li><a href="#whoami" className='header__whoami header__nav__item '>{pageContent ? nav.skills[currentLanguage] : "default"}</a></li>
+                        <li><a href="#portfolio" className='header__portfolio header__nav__item'>{pageContent ? nav.portfolio[currentLanguage] : "default"}</a></li>
+                        <li><a href="#pricing" className='header__pricing header__nav__item'>{pageContent ? nav.pricing[currentLanguage] : "default"}</a></li>
+                        <li><a href="#contact" className='header__contact header__nav__item'>{pageContent ? nav.contact[currentLanguage] : "default"}</a></li>
                         <div className='changeLanguage header__nav__item' onClick={handleLanguage}>
                             <img src={`/icons/${currentLanguage}.svg`} alt='language' className='changeLanguage__icon' />
                         </div>
