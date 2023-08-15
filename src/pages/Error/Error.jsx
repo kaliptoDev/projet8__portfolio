943bb1f3db1570d71f7b28472cf97f8472398852
kaliptoDev <121462067+kaliptoDev@users.noreply.github.com>
2023-08-15 13:18:02 +0200

    Error handling for 404

---
+++ src/pages/Error/Error.jsx
@@ -0,0 +1,14 @@
+import './Error.css'
+
+const Error = () => {
+
+    return (
+        <div className="error">
+            <h1>404</h1>
+            <h2>Page not found</h2>
+            <p>Sorry, the page you're looking for doesn't exist.</p>
+        </div>
+    )
+}
+
+export default Error;
--- src/router/RoutesTree.jsx
+++ src/router/RoutesTree.jsx
@@ -1,14 +1,14 @@
 import {Route, Routes} from 'react-router-dom';
 import Home from '../pages/Home/Home';
 // import About from '../pages/About';
-// import Error from '../pages/Error';
+import Error from '../pages/Error/Error';
 
 const RoutesTree = () => {
 
     return (
         <Routes>
             <Route path="/" element={<Home/>}/>
-            {/* <Route path="*" element={<Error/>}/> */}
+            <Route path="*" element={<Error/>}/>
         </Routes>
     )
 }
