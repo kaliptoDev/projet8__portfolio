5bfc86b5f2ecb1adba1558a9fcd5307559ea8c92
kaliptoDev <121462067+kaliptoDev@users.noreply.github.com>
2023-08-18 14:46:35 +0200

    Temporary Footer added

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import './Footer.css'
+
+const Footer = () => {
+
+    return (
+        <div className="footer">
+            <p className="footer__text">© 2023 Baptiste André - Kalipto</p>
+        </div>
+    )
+
+}
+
+export default Footer
